afc0c6206fde0bd2a2bdad2601b047afbd6b41ba
davideaprea <apredav.developer@gmail.com>
2025-05-21 14:25:04 +0200

    modifica footer

--- js/footer.js
+++ js/footer.js
@@ -50,19 +50,10 @@
             </div>
         </div>
     </div>
-    <div class="container-fluid text-white" style="background: #061429;">
-        <div class="container text-center">
-            <div class="row justify-content-end">
-                <div class="col-lg-8 col-md-6">
-                    <div class="d-flex align-items-center justify-content-center" style="height: 75px;">
-                        <p class="mb-0">&copy; Eris Tech Srl. <span data-key="RIGHTS_RESERVED"></span>. <span data-key="P_IVA"></span>: 09286090965 - <span data-key="SHARE_CAPITAL"></span>: 20.000,00€ i.v. - PEC: eristech@pec.it
-						
-						<!--/*** This template is free as long as you keep the footer author's credit link/attribution link/backlink. If you'd like to use the template without the footer author's credit link/attribution link/backlink, you can purchase the Credit Removal License from "https://htmlcodex.com/credit-removal". Thank you for your support. ***/-->
-						<span data-key="DESIGNED_BY"></span> <a class="text-white border-bottom" href="https://htmlcodex.com">HTML Codex</a></p>
-                    </div>
-                </div>
-            </div>
-        </div>
+    <div class="container-fluid text-white p-4 text-center" style="background: #061429;">
+        <p class="m-1">&copy; Eris Tech Srl. <span data-key="RIGHTS_RESERVED"></span>.</p>
+        <p class="m-1"><span data-key="P_IVA"></span>: 09286090965 | <span data-key="SHARE_CAPITAL"></span>: 20.000,00€ i.v. | PEC: eristech@pec.it</p>
+        <p class="m-1"><span data-key="DESIGNED_BY"></span> <a class="text-white border-bottom" href="https://htmlcodex.com">HTML Codex</a></p>
     </div>
     <!-- Footer End -->
     `;
